ac767e761b60a00dfd76e459c9b397780befacc5
BrianLTJ <739142578@qq.com>
2016-06-07 00:55:15 +0800

    Add app for lists,chapter3.3

--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
-class SimpleTest(self):
+class SimpleTest(TestCase):
 	"""docstring for SimpleTest"""
 	def test_basic_addition(self):
-		
 		self.assertEqual(1 + 1 , 3)
 		
