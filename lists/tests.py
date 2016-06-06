414416009355bc62a211cba946b44804f6de0381
BrianLTJ <739142578@qq.com>
2016-06-07 00:50:05 +0800

    Finish Ch2

---
+++ lists/tests.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+
+# Create your tests here.
+class SimpleTest(self):
+	"""docstring for SimpleTest"""
+	def test_basic_addition(self):
+		
+		self.assertEqual(1 + 1 , 3)
+		
